e2a62bffc4cb4e249a5c1383b8502fd1190657a1
anayet71 <ariyananayet@gmail.com>
2024-12-07 01:10:01 +0600

    nav section added and responsive

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ["./src/**/*.{html,js}"],
+    theme: {
+      extend: {},
+    },
+    plugins: [],
+  }
+  module.exports = {
+    theme: {
+      extend: {
+        fontFamily: {
+          lexend: ['Lexend', 'sans-serif']
+        },
+      },
+    },
+  };
+  
