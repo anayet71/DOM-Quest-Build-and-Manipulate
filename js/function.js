61f361d07d22ebfb8b21c4a3c357acaa607c2476
anayet71 <ariyananayet@gmail.com>
2024-12-07 10:42:06 +0600

    card 1 added

---
+++ js/function.js
@@ -0,0 +1,8 @@
+// document.getElementById("history-btn").addEventListener('click', function(){
+//     document.getElementById('history').classList.remove("hidden")
+//     document.getElementById('donation').classList.add('hidden')
+// })
+// document.getElementById("donation-btn").addEventListener('click', function(){
+//     document.getElementById('history').classList.add("hidden")
+//     document.getElementById('donation').classList.remove('hidden')
+// })
